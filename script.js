e25a0d0034639806fe8c05dcccfe3d4bdf8ab534
Ana Luiza Gouvêa <analuiza.ogouvea@gmail.com>
2023-10-18 22:46:58 -0300

    Jogo Funcionando

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 const palavras = ["animal", "dicas", "moeda"] //array de palavras
-const palavraJogada = palavras[Math.floor(Math.random() * palavras.length)] //floor arredonda pra inteiro, random decimal
 const letrasErradas = [] //vai armazenar as letras erradas
 const letrasCertas = [] //vai armazenas as letras certas
+const images = ["images/6.png", "images/5.png", "images/4.png", "images/3.png", "images/2.png", "images/1.png"]
 
-
+//pega a letra que foi digitada verifica se tá entre a-z
 document.addEventListener("keydown", (evento) =>{ //identifica a tecla apertada
-    const codigo = evento.keyCode //cade letra tem seu cod, entre 65-90 A-Z
-    if(codigo >= 65 && codigo <=90){
-        const letra = evento.key //pega o cod da letra digitada
-        
+    if(/^[a-zA-Z]$/.test(evento.key)){
+        const letra = evento.key.toLowerCase() //coloca tudo em minusculo
+    
         if(letrasErradas.includes(letra) || letrasCertas.includes(letra)){ 
             avisoLetraRepetida()
         }else{
-            if(palavraJogada.includes(letra)){
+            if(palavraSorteada.includes(letra)){
                 letrasCertas.push(letra)//add no array
             }else{
                 letrasErradas.push(letra)//add no arry
@@ -22,56 +21,72 @@ document.addEventListener("keydown", (evento) =>{ //identifica a tecla apertada
     }
 })
 
+//após o jogo ser iniciado troca a escrita do botão
+document.getElementById("btn-iniciar").addEventListener("click", function(){
+    this.textContent = "Jogar Novamente"
+})
+
+//restaura tudo para os valores iniciar
+function iniciarJogo(){
+    palavraSorteada = palavras[Math.floor(Math.random() * palavras.length)] //floor arredonda pra inteiro, random decimal
+    letrasErradas.length = 0
+    letrasCertas.length = 0
+    document.getElementById("forca").src = "images/7.png"
+    mostrarLetrasErradas()
+    desenharForca()
+}
+
 function atualizarJogo(){
     mostrarLetrasErradas()
     mostrarLetrasCertas()
     desenharForca()
-    checarJogo()
+    setTimeout(function(){
+        checarJogo()
+    }, 500)
 }
 
-function mostrarLetrasErradas(){
-    const errada = document.querySelector(".letras-erradas")
-    errada.innerHTML = "<h3>Letras Erradas</h3>"
-    letrasErradas.forEach(letra =>{
-        errada.innerHTML += `<span>${letra}</span>` //${} é uma variavel
-    })
+//pega a qtd de letras da palavra e coloca as linhas
+function desenharLinhas(){
+    const certa = document.querySelector(".palavra-certa")
+    certa.innerHTML = ""
+    var qtd = palavraSorteada.length 
+    for(let i=0; i<qtd; i++){
+        certa.innerHTML += `<span> _ </span>`
+    }
 }
 
 function mostrarLetrasCertas(){
     const certa = document.querySelector(".palavra-certa")
-    certa.innerHTML = " "
-    palavraJogada.split("").forEach(letra =>{ //valida se as letras fazem parte
+    certa.innerHTML = ""
+    palavraSorteada.split("").forEach(letra =>{ //valida se as letras fazem parte
         if(letrasCertas.includes(letra)){
             certa.innerHTML += `<span>${letra}</span>`
         }else{
             certa.innerHTML += `<span> _ </span>`
         }
     })
-    
+}
+
+function mostrarLetrasErradas(){
+    const errada = document.getElementById("letras-erradas")
+    errada.innerHTML = letrasErradas.join(" - ")
 }
 
 function desenharForca(){
-    const partesCorpo = document.querySelectorAll(".forca-parte")
+    const partesCorpo = document.getElementById("forca")
     for(let i=0; i<letrasErradas.length; i++){
-        partesCorpo[i].style.display = "block" //ESSA PARTE QUE TENHO QUE MEXER PRA PODER REALIZAR A SUBSTITUIÇÃO DAS IMAGENS
+        partesCorpo.setAttribute("src", images[i])
     }
 }
 
 function checarJogo(){
-    let mensagem =""
     const certa = document.querySelector(".palavra-certa")
-    const partesCorpo = document.querySelectorAll(".forca-parte")
 
-    if(letrasErradas.length === partesCorpo.length){
-       // alert("Você perdeu")
-        mensagem = "Fim de Jogo"
+    if(letrasErradas.length ==images.length){
+        avisoVocePerdeu()
     }
-    if(palavraJogada === certa.innerText){
-        mensagem = "Você ganhou"
-    }
-    if(mensagem){
-        document.querySelector("#mensagem").innerHTML = mensagem
-        document.querySelector(".popup").style.display = "flex"
+    if(palavraSorteada === certa.innerText){
+        avisoVoceGanhou()
     }
 }
 
@@ -79,6 +94,10 @@ function avisoLetraRepetida(){
     alert("Você já usou essa letra")
 }
 
-function reiniciar(){
-    window.location.reload()
+function avisoVocePerdeu(){
+    alert("VOCÊ PERDEU")
+}
+
+function avisoVoceGanhou(){
+    alert("VOCÊ GANHOU!!!")
 }
